d2363e5e3b01fe9bb0897b81803d4272715b37fb
minhtuan251295 <nguyentienminhtuan25121995@gmail.com>
2021-03-17 15:16:09 +0700

    change chart lib

--- flutter_ai_quant/lib/modules/home_screens/screens/home.dart
+++ flutter_ai_quant/lib/modules/home_screens/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_widgets/responsive_widgets.dart';
+import '../components/high_light_stocks.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -18,62 +19,7 @@ class HomeScreen extends StatelessWidget {
             backgroundColor: Theme.of(context).backgroundColor,
             body: Column(
               children: <Widget>[
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            padding: EdgeInsetsResponsive.fromLTRB(30, 50, 20, 50),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      margin: EdgeInsetsResponsive.fromLTRB(0.0, 0.0, 0.0, 15.0),
-                                      child: Text("S&P 500 Fut",
-                                          style: TextStyle(
-                                            color: Color(0xff539BDC),
-                                            fontFamily: 'Helvetica Neue',
-                                            fontSize: 45.sp,
-                                            letterSpacing: 0,
-                                          )),
-                                    ),
-                                    Container(
-                                      margin: EdgeInsetsResponsive.fromLTRB(0.0, 0.0, 0.0, 15.0),
-                                      child: Text("3,367.75",
-                                          style: TextStyle(
-                                            color: Color(0xffFFFFFF),
-                                            fontFamily: 'Helvetica Neue',
-                                            fontSize: 35.sp,
-                                            letterSpacing: 0,
-                                          )),
-                                    ),
-                                    Container(
-                                      margin: EdgeInsetsResponsive.fromLTRB(0.0, 0.0, 0.0, 5.0),
-                                      child: Text("-15.35 (-0.45%)",
-                                          style: TextStyle(
-                                            color: Color(0xffED6060),
-                                            fontFamily: 'Helvetica Neue',
-                                            fontSize: 40.sp,
-                                            letterSpacing: 0,
-                                          )),
-                                    ),
-                                  ],
-                                ),
-                                SizedBoxResponsive(width: 50),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      Text("Hello", style: TextStyle(color: Colors.white))
-                    ],
-                  ),
-                ),
+                HighLightStocks(),
                 Text("Row for danh mục", style: TextStyle(color: Colors.white)),
                 Text("Row for mã khuyến nghị",
                     style: TextStyle(color: Colors.white)),
