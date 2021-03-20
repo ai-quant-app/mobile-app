0487f490cb5453ff4344d8383b21391195538034
Minh Kim Long <minhkimlong94@gmail.com>
2021-03-20 20:35:42 +0700

    + Commit fix charts in hoem screen

--- flutter_ai_quant/lib/modules/home/components/high_light_stocks.dart
+++ flutter_ai_quant/lib/modules/home/components/high_light_stocks.dart
@@ -17,126 +17,128 @@ class HighLightStocks extends StatelessWidget {
       allowFontScaling: true,
       child: Container(
         height: 350.sp,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsetsResponsive.fromLTRB(30, 50, 20, 50),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: EdgeInsetsResponsive.fromLTRB(
-                            0.0, 0.0, 0.0, 15.0),
-                        child: Text("S&P 500 Fut",
+        child: FittedBox(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsetsResponsive.fromLTRB(30, 50, 20, 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsetsResponsive.fromLTRB(
+                              0.0, 0.0, 0.0, 15.0),
+                          child: Text("S&P 500 Fut",
+                              style: TextStyle(
+                                color: Color(0xff539BDC),
+                                fontFamily: 'Helvetica Neue',
+                                fontSize: 35.sp,
+                                letterSpacing: 0,
+                              )),
+                        ),
+                        Container(
+                          margin: EdgeInsetsResponsive.fromLTRB(
+                              0.0, 0.0, 0.0, 15.0),
+                          child: Text(
+                            "3,367.75",
+                            style: TextStyle(
+                              color: Color(0xffFFFFFF),
+                              fontFamily: 'Helvetica Neue',
+                              fontSize: 25.sp,
+                              letterSpacing: 0,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin:
+                              EdgeInsetsResponsive.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                          child: Text(
+                            "-15.35 (-0.45%)",
+                            style: TextStyle(
+                              color: Color(0xffED6060),
+                              fontFamily: 'Helvetica Neue',
+                              fontSize: 30.sp,
+                              letterSpacing: 0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    // SizedBoxResponsive(width: 10),
+                    ContainerResponsive(
+                      width: MediaQuery.of(context).size.height / 3,
+                      height: 180,
+                      child: SimpleLineChart(
+                          SimpleLineChart.withSampleData().seriesList),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsetsResponsive.fromLTRB(30, 50, 20, 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsetsResponsive.fromLTRB(
+                              0.0, 0.0, 0.0, 15.0),
+                          child: Text(
+                            "VIX",
                             style: TextStyle(
                               color: Color(0xff539BDC),
                               fontFamily: 'Helvetica Neue',
                               fontSize: 35.sp,
                               letterSpacing: 0,
-                            )),
-                      ),
-                      Container(
-                        margin: EdgeInsetsResponsive.fromLTRB(
-                            0.0, 0.0, 0.0, 15.0),
-                        child: Text(
-                          "3,367.75",
-                          style: TextStyle(
-                            color: Color(0xffFFFFFF),
-                            fontFamily: 'Helvetica Neue',
-                            fontSize: 25.sp,
-                            letterSpacing: 0,
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        margin:
-                            EdgeInsetsResponsive.fromLTRB(0.0, 0.0, 0.0, 0.0),
-                        child: Text(
-                          "-15.35 (-0.45%)",
-                          style: TextStyle(
-                            color: Color(0xffED6060),
-                            fontFamily: 'Helvetica Neue',
-                            fontSize: 30.sp,
-                            letterSpacing: 0,
+                        Container(
+                          margin: EdgeInsetsResponsive.fromLTRB(
+                              0.0, 0.0, 0.0, 15.0),
+                          child: Text(
+                            "33.35",
+                            style: TextStyle(
+                              color: Color(0xffFFFFFF),
+                              fontFamily: 'Helvetica Neue',
+                              fontSize: 25.sp,
+                              letterSpacing: 0,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  // SizedBoxResponsive(width: 10),
-                  ContainerResponsive(
-                    width: MediaQuery.of(context).size.height / 3,
-                    height: 180,
-                    child: SimpleLineChart(
-                        SimpleLineChart.withSampleData().seriesList),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsetsResponsive.fromLTRB(30, 50, 20, 50),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: EdgeInsetsResponsive.fromLTRB(
-                            0.0, 0.0, 0.0, 15.0),
-                        child: Text(
-                          "VIX",
-                          style: TextStyle(
-                            color: Color(0xff539BDC),
-                            fontFamily: 'Helvetica Neue',
-                            fontSize: 35.sp,
-                            letterSpacing: 0,
+                        Container(
+                          margin: EdgeInsetsResponsive.fromLTRB(
+                              0.0, 0.0, 0.0, 5.0),
+                          child: Text(
+                            "+ 0.89 (+2.74%)",
+                            style: TextStyle(
+                              color: Color(0xFF25E654),
+                              fontFamily: 'Helvetica Neue',
+                              fontSize: 30.sp,
+                              letterSpacing: 0,
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        margin: EdgeInsetsResponsive.fromLTRB(
-                            0.0, 0.0, 0.0, 15.0),
-                        child: Text(
-                          "33.35",
-                          style: TextStyle(
-                            color: Color(0xffFFFFFF),
-                            fontFamily: 'Helvetica Neue',
-                            fontSize: 25.sp,
-                            letterSpacing: 0,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsetsResponsive.fromLTRB(
-                            0.0, 0.0, 0.0, 5.0),
-                        child: Text(
-                          "+ 0.89 (+2.74%)",
-                          style: TextStyle(
-                            color: Color(0xFF25E654),
-                            fontFamily: 'Helvetica Neue',
-                            fontSize: 30.sp,
-                            letterSpacing: 0,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  ContainerResponsive(
-                    width: MediaQuery.of(context).size.height / 3,
-                    height: 180,
-                    child: SimpleLineChart(
-                      SimpleLineChart.withSampleData().seriesList,
+                      ],
                     ),
-                  )
-                ],
+                    ContainerResponsive(
+                      width: MediaQuery.of(context).size.height / 3,
+                      height: 180,
+                      child: SimpleLineChart(
+                        SimpleLineChart.withSampleData().seriesList,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
