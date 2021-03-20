480b95216806f8afc72d77d03dfffe04d0c78295
Minh Kim Long <minhkimlong94@gmail.com>
2021-03-20 21:31:15 +0700

    + Commit chart screen

--- flutter_ai_quant/lib/modules/chart/screens/chart_screen.dart
+++ flutter_ai_quant/lib/modules/chart/screens/chart_screen.dart
@@ -20,8 +20,54 @@ class _ChartScreenState extends State<ChartScreen> {
     return ResponsiveWidgets.builder(
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.amber,
-          title: Text("Chart Screen"),
+          backgroundColor: Color(0xFF062D52),
+          title: Row(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.calendar_today_outlined,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.history,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.bar_chart,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          actions: [
+            Container(
+              padding: EdgeInsetsResponsive.all(25),
+              child: OutlinedButton(
+                child: Text(
+                  "GIAO DỊCH",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.sp,
+                  ),
+                ),
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(
+                    color: Color(0xFF074784),
+                    width: 8.sp,
+                  ),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(25.sp))),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ],
         ),
       ),
     );
